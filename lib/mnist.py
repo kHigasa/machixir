e104e27a936856a478e4d63aeae0535472de4e5e
kHigasa <kojihigasa@gmail.com>
2018-10-24 23:06:34 +0900

    barch process

--- lib/mnist.py
+++ lib/mnist.py
@@ -34,13 +34,12 @@ def out():
     x, t = get_data()
     network = init_network()
 
+    batch_size = 100
     accuracy_cnt = 0
-    for i in range(len(x)):
-        y = predict(network, x[i])
-        p = np.argmax(y)
-
-        if p == t[i]:
-            accuracy_cnt += 1
+    for i in range(0, len(x), batch_size):
+        y = predict(network, x[i:i+batch_size])
+        p = np.argmax(y, axis=1)
+        accuracy_cnt += np.sum(p == t[i:i+batch_size])
 
     print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
 
