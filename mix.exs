c66504b70a952515dcf60f0ef4ec6dce45a9d93b
kHigasa <kojihigasa@gmail.com>
2018-11-11 19:56:22 +0900

    Update phoenix

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Machixir.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.0-rc"},
+      {:phoenix, "~> 1.4.0"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
